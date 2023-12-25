63ad9d9e9fc3058ab535dbf59256869baa15e6c7
Manish Sharma <sharmakmanish1@gmail.com>
2023-12-25 23:33:04 +0530

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Login from "./Login";
+function App() {
+  const [id, setId] = useState();
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  );
+}
+
+export default App;
